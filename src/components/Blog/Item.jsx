7bcbebfacd56f00863f6a9cd6d96e6057761a290
oladee <deemajor230600@gmail.com>
2024-06-29 16:16:10 +0100

    included linkedin newsletter

--- src/components/Blog/Item.jsx
+++ src/components/Blog/Item.jsx
@@ -1,30 +1,32 @@
+import { Link } from "react-router-dom"
+import angleArrow from "../../assets/blog/angleArrow.png"
 
-const Item = ({tag, img, topic, subtopic, date }) => {
+const Item = ({tag, img, topic, subtopic, date, to }) => {
   return ( 
-    <div>
-      <div className="w-full max-w-[390px]">
-        <img src={img} alt="" />
+    <Link to={to} target="_blank" className="w-full max-w-[384px]">
+      <div className=" ">
+        <img src={img} alt="" className="h-full max-h-[240px] w-full"/>
       </div>
-      <h4 className=" mt-5 lg:mt-8 mb-3 lg:mb-6 text-xs md:text-sm text-[#3538CD] bg-[#EEF4FF] font-inter py-1 px-2">
+      <h4 className=" mt-5 lg:mt-8 mb-3 lg:mb-6 text-xs md:text-sm text-[#3538CD] bg-[#EEF4FF] font-inter py-1 px-2 w-fit">
         {tag || 'Tag'}
       </h4>
-      <div>
+      <div className="flex">
         <div>
-            <h2 className="text-lg md:text-2xl text-[#1A1A1A] font-bold font-satoshi ">
+            <h2 className="text-lg md:text-2xl text-[#1A1A1A] font-bold font-satoshi pb-2">
                 {topic}
             </h2>
-            <h5 className="text-[#667085] pb-2 md:pb-3 text-wrap">
+            <h5 className="text-[#667085] pb-2 text-wrap">
                 {subtopic}
             </h5>
-            <p>
+            <p className="font-inter text-xs text-[#6941C6] font-semibold">
                 {date}
             </p>
         </div>
         <div>
-            <img src="" alt="" />
+            <img src={angleArrow} alt="" />
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
